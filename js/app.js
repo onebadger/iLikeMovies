27b7c6963690c674e9f5dbb3ffcb7c6fa1e5f51b
onebadger <yakov.barsukov@gmail.com>
2017-10-14 17:10:59 +0300

    files update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 var listHtml = '';
+var filmRating;
 
 // films list concatination
 for (var i = 0 ; i < films.length; ++i) {
-  listHtml += '<h2>' + films[i].name + '</h2>';
+  
+  if (films[i].rating === 5) {
+    filmRating = 'stars5';
+  } else if (films[i].rating === 4) {
+    filmRating = 'stars4';
+  } else if (films[i].rating === 3) {
+    filmRating = 'stars3';
+  } else if (films[i].rating === 2) {
+    filmRating = 'stars2';
+  } else if (films[i].rating === 1) {
+    filmRating = 'stars1';
+  }
+  
+  listHtml += '<h2 class="' + filmRating + '">' + films[i].name + '</h2>';
 }
 
 // append to #films-list
